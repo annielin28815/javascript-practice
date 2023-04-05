9675b7218bdc35c7552c9a3661fee1486ddae9b6
Annie <annielin28815@gmail.com>
2023-04-05 23:56:33 +0800

    number practice

---
+++ 10.number_method/main.js
@@ -0,0 +1,8 @@
+let pi = Math.PI();
+let exponential = Math.E(); // 自然常數
+
+let ceil = Math.ceil(3.56789);  // 無條件進位
+let floor = Math.floor(3.56789);  // 無條件捨棄
+let round = Math.round(3.56789);  // 四捨五入
+
+let absoulte = Math.abs(3.56789); // 取絕對值
